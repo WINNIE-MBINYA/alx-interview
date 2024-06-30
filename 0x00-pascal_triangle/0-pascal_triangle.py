786ec4f5022c31528fa10450f709902455755dc2
WINNIE-MBINYA <winnymbinyamwanza@gmail.com>
2024-06-30 17:05:38 +0300

    Fixpycodestyle errors

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -12,21 +12,21 @@ def pascal_triangle(n):
         n (int): Number of rows of Pascal's triangle to generate.
 
     Returns:
-        list of lists: A list containing lists of integers representing each row
-                       of Pascal's triangle up to the nth row. Returns an empty list
+        list of lists: A list of integers representing each row.
+                       of Pascal's triangle up to the nth row.
                        if n <= 0.
     """
     if n <= 0:
         return []
 
-    triangle = [[1]]  # Initialize with the first row of Pascal's triangle
-    
+    triangle = [[1]]
+
     for i in range(1, n):
-        row = [1]  # Start each row with 1
+        row = [1]
         for j in range(1, i):
             """Calculate each element based on the previous row"""
             row.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
-        row.append(1)  # End each row with 1
-        triangle.append(row)  # Add the row to the triangle
-    
+        row.append(1)
+        triangle.append(row)
+
     return triangle
