c671e4ae54003cbc56f316bfefd498ad3624d551
WINNIE-MBINYA <winnymbinyamwanza@gmail.com>
2024-08-09 01:56:33 +0300

    Change code output

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,27 +1,54 @@
 #!/usr/bin/node
 
+/**
+ * Script that fetches and prints the names of all characters 
+ * from a specific Star Wars movie using the Star Wars API.
+ *
+ * Usage: ./0-starwars_characters.js <movie_id>
+ *
+ * Example: ./0-starwars_characters.js 3
+ * This will print the names of all characters in "Return of the Jedi".
+ */
+
 const request = require('request');
+const API_URL = 'https://swapi-api.alx-tools.com/api';
 
-const movieId = process.argv[2];
-const url = `https://swapi.dev/api/films/${movieId}/`;
+if (process.argv.length > 2) {
+  const movieId = process.argv[2];
 
-request(url, (error, response, body) => {
-  if (error) {
-    console.error('Error fetching data:', error);
-    return;
-  }
+  // Request the film data from the API
+  request(`${API_URL}/films/${movieId}/`, (err, response, body) => {
+    if (err) {
+      return console.error(err);
+    }
+    if (response.statusCode === 200) {
+      // Parse the response body to get character URLs
+      const charactersURLs = JSON.parse(body).characters;
 
-  const data = JSON.parse(body);
-  const characters = data.characters;
+      // Create an array of promises to fetch character names
+      const characterPromises = charactersURLs.map(url => {
+        return new Promise((resolve, reject) => {
+          request(url, (error, res, charBody) => {
+            if (error) {
+              reject(error);
+            } else {
+              // Resolve the promise with the character's name
+              resolve(JSON.parse(charBody).name);
+            }
+          });
+        });
+      });
 
-  characters.forEach((characterUrl) => {
-    request(characterUrl, (err, res, charBody) => {
-      if (err) {
-        console.error('Error fetching character data:', err);
-        return;
-      }
-      const characterData = JSON.parse(charBody);
-      console.log(characterData.name);
-    });
+      // Wait for all promises to resolve and print the names
+      Promise.all(characterPromises)
+        .then(names => {
+          names.forEach(name => console.log(name));
+        })
+        .catch(error => console.error(error));
+    } else {
+      console.error(`Failed to get film data: Status code ${response.statusCode}`);
+    }
   });
-});
+} else {
+  console.log('Usage: ./0-starwars_characters.js <movie_id>');
+}
