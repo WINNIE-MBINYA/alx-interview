ecf3bbffb42f684aec05fc8fd119a98cb78b0952
WINNIE-MBINYA <winnymbinyamwanza@gmail.com>
2024-08-09 01:52:52 +0300

    Change code output

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,31 +1,27 @@
 #!/usr/bin/node
+
 const request = require('request');
 
 const movieId = process.argv[2];
-
-if (!movieId) {
-  console.error('Please provide a movie ID as an argument.');
-  process.exit(1);
-}
-
-const url = `https://swapi.dev/api/films/${movieId}`;
+const url = `https://swapi.dev/api/films/${movieId}/`;
 
 request(url, (error, response, body) => {
-  if (error || response.statusCode !== 200) {
-    console.error('Error fetching movie data:', error);
-    process.exit(1);
+  if (error) {
+    console.error('Error fetching data:', error);
+    return;
   }
 
-  const movie = JSON.parse(body);
-  movie.characters.forEach(characterUrl => {
-    request(characterUrl, (error, response, characterBody) => {
-      if (error || response.statusCode !== 200) {
-        console.error('Error fetching character data:', error);
-        process.exit(1);
-      }
+  const data = JSON.parse(body);
+  const characters = data.characters;
 
-      const character = JSON.parse(characterBody);
-      console.log(character.name);
+  characters.forEach((characterUrl) => {
+    request(characterUrl, (err, res, charBody) => {
+      if (err) {
+        console.error('Error fetching character data:', err);
+        return;
+      }
+      const characterData = JSON.parse(charBody);
+      console.log(characterData.name);
     });
   });
 });
